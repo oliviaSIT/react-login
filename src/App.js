146831436ae1c30f3e8995b79c574e94cfe19984
oliviaSIT <olivia.lee8321@gmail.com>
2022-10-04 16:28:19 -0700

    rename

--- src/App.js
+++ src/App.js
@@ -1,77 +1,34 @@
-import React, { useState } from "react";
-import LoginForm from "./components/LoginForm"
+import React, { Component } from 'react';
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home/Home.js';
+import LoginPage from './components/Login/LoginPage';
 
-function App() {
-  const adminUser = {
-    email: "admin@admin.com",
-    password: "1234"
-  }
 
-  const [user, setUser] = useState({name: "", email: ""});
-  const [error, setError] = useState("");
-
-  const Login = details => {
-    console.log("call Login api");
-    console.log(details);
-
-    // if (details.email == adminUser.email && details.password == adminUser.password) {
-    //   console.log("logged in");
-    //   setUser({
-    //     name: details.name,
-    //     email: details.email
-    //   });
-    // } else {
-    //   console.log("details not match");
-    //   setError("details not match");
-    // }
-
-    setError("");
-
-    const url = "http://localhost:8080/auth/login"; 
-
-    const reqBody = {
-      name: details.name,
-      password: details.password,
-    };
-
-    fetch('url', {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "get",
-      //body: JSON.stringify(reqBody)
-    }).then((response) => {
-      console.log(JSON.stringify(response));
-      if (response.status === 200) {
-        console.log("logged in");
-        setUser({
-          name: details.name,
-          email: details.email
-        });
-      } else {
-        console.log("details not match");
-        setError("details not match");
-      }
-    })
-  }
-
-  const Logout = () => {
-    console.log("logout");
-    setUser({name: "", email: ""});
-  }
-
-  return (
-    <div className="App">
-      {(user.email != "") ? (
-        <div className="welcome">
-          <h2>welcome, <span>{user.name}</span></h2>
-          <button onClick={Logout}>Logout</button>
-        </div>
-      ) : (
-        <LoginForm Login={Login} error={error}/>
-      )}
-    </div>
-  );
+class App extends Component {
+render() {
+	return (
+	<Router>
+		<div className="App">
+			<ul className="App-header">
+			<li>
+				<Link to="/">Home</Link>
+			</li>
+			<li>
+				<Link to="/LoginPage">Login</Link>
+			</li>
+			{/* <li>
+				<Link to="/Algorithm">Algorithm</Link>
+			</li> */}
+			</ul>
+		<Routes>
+				<Route exact path='/' element={< Home />}></Route>
+				<Route exact path='/LoginPage' element={< LoginPage />}></Route>
+				{/* <Route exact path='/Algorithm' element={< Algorithm />}></Route> */}
+		</Routes>
+		</div>
+	</Router>
+);
+}
 }
 
 export default App;
---
+++ src/components/Login/LoginPage.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import LoginForm from "./LoginForm"
+
+function LoginPage() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "1234"
+  }
+
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log("call Login api");
+    console.log(details);
+
+    // if (details.email == adminUser.email && details.password == adminUser.password) {
+    //   console.log("logged in");
+    //   setUser({
+    //     name: details.name,
+    //     email: details.email
+    //   });
+    // } else {
+    //   console.log("details not match");
+    //   setError("details not match");
+    // }
+
+    setError("");
+
+    const url = "http://localhost:8080/auth/login"; 
+
+    const reqBody = {
+      name: details.name,
+      password: details.password,
+    };
+
+    fetch('url', {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "get",
+      //body: JSON.stringify(reqBody)
+    }).then((response) => {
+      console.log(JSON.stringify(response));
+      if (response.status === 200) {
+        console.log("logged in");
+        setUser({
+          name: details.name,
+          email: details.email
+        });
+      } else {
+        console.log("details not match");
+        setError("details not match");
+      }
+    })
+  }
+
+  const Logout = () => {
+    console.log("logout");
+    setUser({name: "", email: ""});
+  }
+
+  return (
+    <div className="LoginPage">
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2>welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error}/>
+      )}
+    </div>
+  );
+}
+
+export default LoginPage;
