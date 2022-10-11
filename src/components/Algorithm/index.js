088e5eca94ffb7a5d8c4e98ea1d415932d680283
oliviaSIT <olivia.lee8321@gmail.com>
2022-10-11 15:46:19 -0700

    display algorithm connect with back end

--- src/components/Algorithm/index.js
+++ src/components/Algorithm/index.js
@@ -1,8 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function AlgorithmPage() {
-    const sample1 = {id: 1, title: "test", company: "Microsoft", tag: "tree"};
-    const [algorithms, setAlgorithms] = useState([sample1]);
+    //const sample1 = {id: 1, title: "test", company: "Microsoft", tag: "tree"};
+    const [algorithms, setAlgorithms] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/algorithms/findAllAlgorithm', {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "get",
+      }).then((response) => {
+        console.log(response)
+        return response.json();
+      }).then((json) => {
+        console.log("before set data")
+        console.log(json);
+        setAlgorithms(json);
+      })
+    }, []);
 
     const DisplayData = algorithms.map(
         (info)=>{
